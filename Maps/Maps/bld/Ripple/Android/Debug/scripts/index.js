43e0b1089276c00f3b4323a6d57419e9de49ff21
Arjaie Alejandro <aalejandro@mymail.mapua.edu.ph>
2015-03-08 14:02:29 +0800

    Partial Hotels Display

--- Maps/Maps/bld/Ripple/Android/Debug/scripts/index.js
+++ Maps/Maps/bld/Ripple/Android/Debug/scripts/index.js
@@ -2,11 +2,15 @@
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
+var map = null;
+var searchManager = null;
 (function () {
     "use strict";
 
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-    document.addEventListener('deviceready', initializeMap, false);
+    document.addEventListener('deviceready', getMap, false);
+    //document.addEventListener('deviceready', LoadSearchModule, false);
+    document.addEventListener('deviceready', searchRequest, false);
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
@@ -23,14 +27,73 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
-    function initializeMap() {
-        // this is where the custom code will go for each mapping implementation
-        var mapOptions = {
-            credentials: "AmSAGzZ9ynbNtNEyfJ8wdkqZLwoxt1KGvW8WGgnboLHCPz9m8hZjzPoxjvJR8trg",
-            mapTypeId: Microsoft.Maps.MapTypeId.road,
-            center: new Microsoft.Maps.Location(43.069452, -89.411373),
-            zoom: 11
-        };
-        var map = new Microsoft.Maps.Map(document.getElementById("map"), mapOptions);
+
+    function getMap() {
+        map = new Microsoft.Maps.Map(document.getElementById('map'), { credentials: 'AmSAGzZ9ynbNtNEyfJ8wdkqZLwoxt1KGvW8WGgnboLHCPz9m8hZjzPoxjvJR8trg' });
+    }
+    function createSearchManager() {
+        map.addComponent('searchManager', new Microsoft.Maps.Search.SearchManager(map);
+        searchManager = map.getComponent('searchManager');
+    }
+    function LoadSearchModule() {
+        Microsoft.Maps.loadModule('Microsoft.Maps.Search', { callback: searchRequest })
+    }
+    function searchRequest() {
+        createSearchManager();
+        var userData = { name: 'User', id: 'XYZ' };
+        var query = 'hotels in Metro Manila, Philippines';
+        var request =
+            {
+                query: query,
+                count: 10,
+                startIndex: 0,
+                bounds: map.getBounds(),
+                callback: search_onSearchSuccess,
+                errorCallback: search_onSearchFailure,
+                userData: userData
+            };
+        searchManager.search(request);
+    }
+    function search_onSearchSuccess(result, userData) {
+        map.entities.clear();
+        var searchResults = result && result.searchResults;
+        if (searchResults) {
+            for (var i = 0; i < searchResults.length; i++) {
+                search_createMapPin(searchResults[i]);
+            }
+            if (result.searchRegion && result.searchRegion.mapBounds) {
+                map.setView({ bounds: result.searchRegion.mapBounds.locationRect });
+            }
+            else {
+                alert('No results returned, Please try after sometime.');
+            }
+        }
+    }
+    function search_createMapPin(result) {
+        if (result) {
+            var pin = new Microsoft.Maps.Pushpin(result.location, null);
+            Microsoft.Maps.Events.addHandler(pin, 'click', function () { search_showInfoBox(result) });
+            map.entities.push(pin);
+        }
+    }
+    function search_showInfoBox(result) {
+        if (currInfobox) {
+            currInfobox.setOptions({ visible: true });
+            map.entities.remove(currInfobox);
+        }
+        currInfobox = new Microsoft.Maps.Infobox(
+            result.location,
+            {
+                title: result.name,
+                description: [result.address, result.city, result.state, result.country, result.phone].join(' '),
+                showPointer: true,
+                titleAction: null,
+                titleClickHandler: null
+            });
+        currInfobox.setOptions({ visible: true });
+        map.entities.push(currInfobox);
+    }
+    function search_onSearchFailure(result, userData) {
+        alert('Search failed');
     }
 } )();
